50bdc62de0b7243235b168e5c2fdf716835d184e
Samriddhi Kapoor <samriddhik4@gmail.com>
2026-07-19 15:34:47 +0530

    Add add.py

---
+++ Python Training/add.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+result = num1 + num2
+print(f"The result of adding {num1} and {num2} is: {result}")
+
+result = num1 - num2
+print(f"The result of subtracting {num1} and {num2} is: {result}")
+
+result = num1 * num2
+print(f"The result of multiplying {num1} and {num2} is: {result}")
+
+result = num1 / num2
+print(f"The result of dividing {num1} and {num2} is: {result}")
