255a6bafa828bbaddc9c9805d65534b9d0b8fa43
samriddhik04 <139965484+samriddhik04@users.noreply.github.com>
2026-07-22 21:07:08 +0530

    Delete Python Training directory

--- Python Training/Day 3/1-log.py
+++
@@ -1,7 +0,0 @@
-import logging
-# logging.basicConfig(level=logging.DEBUG)
-# logging.debug("Variable value = 25")
-
-
-logging.basicConfig(level=logging.INFO)
-logging.info("Employee Added Successfully")
--- Python Training/vowel.py
+++
@@ -1,6 +0,0 @@
-str = input("Enter a Char: ")
-vowel = str.strip().lower()
-if vowel in 'aeiou':
-    print(f"'{vowel}' is a vowel.")
-else:
-    print(f"'{vowel}' is not a vowel.")
